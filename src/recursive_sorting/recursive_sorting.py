622eff0803bda6848dca4ba3d1c3dedd5d011b7e
taterntots <matthew.a.weidner@gmail.com>
2020-04-14 18:33:23 -0400

    completed the recursive_sorting algo

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -18,18 +18,34 @@ def quick_sort(arr):
     return quick_sort(smaller) + [pivot] + quick_sort(larger)
 
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    merged_arr2 = []
-    print(elements)
-    for a in arrA:
-        for b in arrB:
-            if arrA[a] <= arrB[b]:
-                print('yes!')
-                merged_arr2.append(arr[a])
-                print(merged_arr2)
-    
+    # print('-----------------------')
+    # print(f'arrA: ', arrA)
+    # print(f'arrB: ', arrB)
+
+    i = 0
+    while len(arrA) > 0 and len(arrB) > 0:
+        if arrA[0] < arrB[0]:
+            merged_arr[i] = arrA[0]
+            del (arrA[0])
+            i = i + 1
+        else:
+            merged_arr[i] = arrB[0]
+            del (arrB[0])
+            i = i + 1
+    if len(arrA) > 0:
+        for x in arrA:
+            merged_arr[i] = x
+            i = i + 1
+    elif len(arrB) > 0:
+        for x in arrB:
+            merged_arr[i] = x
+            i += 1
+
     return merged_arr
 
 
@@ -39,16 +55,16 @@ def merge_sort(arr):
     if len(arr) <= 1:
         return arr
 
-    # Split the arrays into half
-    arr1 = []
-    arr2 = []
+    # Split the array in half
+    half = len(arr) // 2
+    first_half = merge_sort(arr[:half])
+    second_half = merge_sort(arr[half:])
+    # print(f'First Half: {first_half}')
+    # print(f'Second Half: {second_half}')
 
-    # Sort each half
-    arr1 = [0,1,2,3,6]
-    arr2 = [4,5,7,8,9]
-    # Merge them back together
-    # Compare the first values of each array, add smaller of the 2 to result
-    merged = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # Merge them back together in order with sorted merge function
+    arr = merge(first_half, second_half)
+
 
     return arr
 
@@ -59,7 +75,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -67,20 +84,23 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
+
 
 # ----------------------------------------------------------
 # ------------------- Data to play with --------------------
 # ----------------------------------------------------------
 arr = [5, 1, 7, 8, 2, 95, 79, 4, 11, 74, 34, 41]
-arrA = [1, 2, 4, 5, 8]
-arrB = [3, 6, 7, 9, 10]
+arrA = [24, 22, 76, 56, 8]
+arrB = [27, 6, 7, 9, 10]
 
 print(f'Original =', arr)
 arr = [5, 1, 7, 8, 2, 95, 79, 4, 11, 74, 34, 41]
 print(f'SortQuick =', quick_sort(arr))
 arr = [5, 1, 7, 8, 2, 95, 79, 4, 11, 74, 34, 41]
+print('----------------------------------------------')
 print(f'SortMerge =', merge_sort(arr))
 print(f'MergeTwo =', merge(arrA, arrB))
+print('----------------------------------------------')
